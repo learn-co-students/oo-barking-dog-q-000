31b2fe6b8a54268fd6143515624043c7b215e437
Louiefigz <figueroa.aaron@gmail.com>
2015-10-17 05:39:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog
+
+  def name=(string)
+    @name=string
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(string)
+     @bark=string
+  end
+
+def bark
+  @bark = puts "woof!"
+end
+
+end
+
