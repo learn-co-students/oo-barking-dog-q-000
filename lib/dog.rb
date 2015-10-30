d8d87d9d340bc12a7cd3f5f384ca99cba05ea094
AdventuresWithJerricko <adventureswithjerricko@gmail.com>
2015-10-30 04:31:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Dog
+  def initialize
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+# Test the Dog class
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+fido.bark
+
+
