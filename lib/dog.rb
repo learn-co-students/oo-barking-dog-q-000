9dcb89a3eab20473ef113fb12fdfc4a82fc2cae5
aenser <aaronenser1@gmail.com>
2015-11-06 14:55:46 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog 
+end 
+
+class Dog 
+  def name 
+    @name 
+  end 
+  def name=(new_name)
+    @name=new_name 
+  end
+end 
+
+class Dog 
+  def bark
+    puts "woof!"
+  end 
+end 
+
