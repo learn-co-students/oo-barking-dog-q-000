a40726e7b1c00de84182fa86b09dac150b638206
Wallace Donegan <Wallace.Donegan@gmail.com>
2016-01-03 06:49:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  @name = nil
+  def initialize(name='')
+    @name = name
+  end
+
+  def name()
+    return @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def bark()
+    puts 'woof!'
+  end
+end
