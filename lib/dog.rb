acc1e1808ff0646261db721c114f443b305b400a
jes708 <jonschwarz@nyu.edu>
2016-01-24 22:16:53 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    @bark = "woof!"
+    puts @bark
+  end
+  
+end
