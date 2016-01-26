e4dde9711b610bb19fa95f6b654bd4a7395c0db7
CodeByLine <yleventhal@ymail.com>
2016-01-26 00:28:01 +0000

    nitrous-backup at Tue Jan 26 00:28:00 UTC 2016

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 # Your code goes here!
 class Dog
-#   def initialize(dogs_name)
+  #   def initialize(dogs_name) #NOT AT ALL necessary!
 #     @name = dogs_name
 #   end
 
@@ -9,7 +9,7 @@ class Dog
   end
 
   def name
-    @name 
+    @name
   end
 
   def bark
