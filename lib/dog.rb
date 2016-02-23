8e64d6e57e25aef1afa027530aa1021f4bc3a9db
miclp <michaelpeimani@gmail.com>
2016-02-22 18:55:06 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+# Dog class
+
+class Dog
+  def initialize() # this had a num args error so changed to ()
+    #@name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
