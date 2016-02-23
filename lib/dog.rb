290400ceb3bdf36ee97d42777708a0b321c6b372
miclp <michaelpeimani@gmail.com>
2016-02-23 13:28:48 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,15 @@
 # Dog class
 
 class Dog
-  def initialize() # this had a num args error so changed to ()
-    #@name = name
+
+  def initialize(name = nil)
+    @name = name
   end
+
   def name
     @name
   end
+
   def name=(new_name)
     @name = new_name
   end
