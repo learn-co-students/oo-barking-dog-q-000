5d04db3ca992a4622592c163f7a8af6da80ef3a1
rambutan25 <limarman895@gmail.com>
2016-03-19 14:43:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+#use a setter and getter the write a method
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
+
+
