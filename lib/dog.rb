864f7a25e48e00063c8aa69c92fe23150963fd4b
scottibe <bewick.scott@gmail.com>
2016-04-10 19:18:35 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Your code goes here!
+  class Dog 
+  
+  def name=(dogs_name)
+    @my_dogs_name = dogs_name
+  end
+  
+  def name
+    @my_dogs_name
+  end 
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+puts fido.bark
+
+
+
+
+
+
+
+  # def initialize(name, bark)
+  #   @name = name
+  #   @bark = bark
+  # end 
+
+
+# fido.name = "Fido"
+# fido.bark = "woof!"
+# puts fido.bark
