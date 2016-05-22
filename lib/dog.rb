260a23736d5dbd7afbbda27967618f44b3c2100d
Peregrin Garet <peregringaret@gmail.com>
2016-05-22 14:17:45 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  def name=(param)
+    @name = param
+  end
+  def name
+    return @name
+  end
+  def dName
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
