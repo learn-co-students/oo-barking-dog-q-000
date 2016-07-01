5f3a1ac79b817f95a33b6dc87e170cc351e2d341
John Ballon <ballonj1@tcnj.edu>
2016-07-01 23:03:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
+
+spot = Dog.new
+spot.name = "Spot"
+spot.bark
