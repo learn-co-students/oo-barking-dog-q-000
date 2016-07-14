c1aa43f133f06a1d0653d88e96c2c76c99ff0ff5
Kenlyn Terai <kkterai@gmail.com>
2016-07-14 06:03:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog
+  def initialize
+    @name = name
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
+
+class Dog
+  def bark
+    puts "woof!"
+  end
+end
