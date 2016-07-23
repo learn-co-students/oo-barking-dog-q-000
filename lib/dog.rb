9b3fb1a30b3fde6ec74d592a5ee7ec9139c1e13b
Melissa Balch <melissabalch@gmail.com>
2016-07-23 04:34:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+  attr_accessor :name
+
+  def initialize(name="name")
+    @name = name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
